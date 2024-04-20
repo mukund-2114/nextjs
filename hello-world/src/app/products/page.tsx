cdf012f68817d2e7ccea06c2aa13df01057abe20
Mukund Kapadia <mdrkkapadia@gmail.com>
2024-04-20 17:58:34 -0400

    links

--- hello-world/src/app/products/page.tsx
+++ hello-world/src/app/products/page.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
+
 export default function Products(){
     return(
         <>
             <h1>Product List</h1>
-            <h3>Product 1</h3>
-            <h3>Product 2</h3>
-            <h3>Product 3</h3>
+            <Link href={'/'}>Home</Link>
+            <h2><Link href={'/products/1'}>Product 1</Link></h2>
+            <h2><Link href={'/products/2'}>Product 2</Link></h2>
+            <h2><Link href={'/products/3'}>Product 3</Link> </h2>
         </>
     )
 }
